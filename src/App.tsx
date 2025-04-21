b766be6531fe6d91105c05ee113ae18fab94140d
Amil <amilseferov305@gmail.com>
2025-04-21 17:12:38 +0400

    add

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import "./App.css";
+
+function App() {
+  const divs: any = [];
+  for (let i: number = 0; i < 10; i++) {
+    divs.push(
+      <div
+        className="bg-stone-800 w-[55px] h-[30px] rounded-full text-white grid justify-center"
+        key={i}
+      >
+        {" "}
+        {i + 1}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="w-full h-screen grid justify-center items-center bg-gradient-to-b from-sky-400 to-blue-800">
+        <div className="w-[460px] h-[400px] bg-stone-900 rounded-[10px] ">
+          <div className="h-[100px] w-full p-[10px] rounded-[10px]"></div>
+          <div className="grid grid-cols-2">
+            <div className="w-full grid grid-cols-3 gap-4 p-[10px] ">{divs}</div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
