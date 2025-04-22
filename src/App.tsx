4575a8255505fc67aaf899f1c9ef6592acc7d9ea
Amil <amilseferov305@gmail.com>
2025-04-22 18:05:08 +0400

    delete buton added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import { LuDelete } from "react-icons/lu";
 function App() {
   const divs: any = [];
   for (let i: number = 0; i < 9; i++) {
@@ -16,11 +16,27 @@ function App() {
 
   return (
     <>
-      <div className="w-full h-screen grid justify-center items-center bg-gradient-to-b from-sky-400 to-blue-800">
+      <div className="w-full  h-screen grid justify-center items-center bg-gradient-to-b from-sky-400 to-blue-800">
         <div className="w-fit h-fit bg-stone-900 rounded-[10px] grid-rows-2 ">
-          <div className="h-[100px] w-full p-[10px] rounded-[10px]"></div>
-          <div className="grid grid-cols-[3fr_1fr] p-[10px]">
-            <div className="w-full grid grid-cols-3 gap-3 py-[10px] justify-center ">
+          <div className="h-[100px] w-full p-[10px] rounded-[10px]">
+            <p className="text-white">0</p>
+          </div>
+          <div className="grid grid-cols-[3fr_1fr] cursor-pointer grid-rows-[1fr_4fr] p-[10px]">
+            <div className="grid grid-cols-4 gap-x-3 row-1 col-span-2">
+              <div className="bg-orange-600 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center ">
+                AC
+              </div>
+              <div className="bg-orange-600 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center ">
+                !
+              </div>
+              <div className="bg-orange-600 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center ">
+                %
+              </div>
+              <div className="bg-orange-600 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center ">
+                <LuDelete/>
+              </div>
+            </div>
+            <div className="w-full grid grid-cols-3 row-2 gap-3  justify-center ">
               {divs}
               <div className="bg-orange-500 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center col-1">
                 .
@@ -28,11 +44,11 @@ function App() {
               <div className="bg-stone-800 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center col-2">
                 0
               </div>
-              <div className="bg-orange-700 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center col-3">
+              <div className="bg-orange-900 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center col-3">
                 =
               </div>
             </div>
-            <div className="grid grid-cols-1 gap-3 py-[10px]">
+            <div className="grid grid-cols-1 row-2 gap-3 ">
               <div className="bg-orange-500 w-[55px] h-[30px] rounded-full text-white grid justify-center col-2">
                 -
               </div>
@@ -45,7 +61,6 @@ function App() {
               <div className="bg-orange-500 w-[55px] h-[30px] rounded-full text-white grid justify-center col-2">
                 x
               </div>
-             
             </div>
           </div>
         </div>
