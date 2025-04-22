f5a7faf88499cf76cea5138f9792b380aebd8061
Amil <amilseferov305@gmail.com>
2025-04-22 17:13:18 +0400

    operations added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   for (let i: number = 0; i < 9; i++) {
     divs.push(
       <div
-        className="bg-stone-800 w-[55px] h-[30px] rounded-full text-white grid justify-center"
+        className="bg-stone-800 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center"
         key={i}
       >
         {" "}
@@ -19,12 +19,34 @@ function App() {
       <div className="w-full h-screen grid justify-center items-center bg-gradient-to-b from-sky-400 to-blue-800">
         <div className="w-fit h-fit bg-stone-900 rounded-[10px] grid-rows-2 ">
           <div className="h-[100px] w-full p-[10px] rounded-[10px]"></div>
-          <div className="grid grid-cols-2">
-            <div className="w-full grid grid-cols-3 gap-3 p-[10px] justify-center ">
+          <div className="grid grid-cols-[3fr_1fr] p-[10px]">
+            <div className="w-full grid grid-cols-3 gap-3 py-[10px] justify-center ">
               {divs}
-              <div className="bg-stone-800 w-[55px] h-[30px] rounded-full text-white grid justify-center col-2">0</div>
+              <div className="bg-orange-500 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center col-1">
+                .
+              </div>
+              <div className="bg-stone-800 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center col-2">
+                0
+              </div>
+              <div className="bg-orange-700 w-[55px] h-[30px] rounded-full text-white grid justify-center items-center col-3">
+                =
+              </div>
             </div>
-            <div></div>
+            <div className="grid grid-cols-1 gap-3 py-[10px]">
+              <div className="bg-orange-500 w-[55px] h-[30px] rounded-full text-white grid justify-center col-2">
+                -
+              </div>
+              <div className="bg-orange-500 w-[55px] h-[30px] rounded-full text-white grid justify-center col-2">
+                +
+              </div>
+              <div className="bg-orange-500 w-[55px] h-[30px] rounded-full text-white grid justify-center col-2">
+                :
+              </div>
+              <div className="bg-orange-500 w-[55px] h-[30px] rounded-full text-white grid justify-center col-2">
+                x
+              </div>
+             
+            </div>
           </div>
         </div>
       </div>
